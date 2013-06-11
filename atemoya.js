73f86028df570c3d063b625c98b895201171548b
Igor Entaltsev <igor.yentaltsev@gmail.com>
2013-06-11 10:27:41 -0400

    131011t 10:27 - a bunch of changes

--- atemoya.js
+++ atemoya.js
@@ -1,3 +1,4 @@
+
 // 1 
 const BASE_URL = "https://www.transifex.com/api/2/project/";
 const DEFAULT_DIR = 'locale';
@@ -7,33 +8,21 @@ const DEFAULT_PROJECT = 'amore';
 var fs = require('fs'),
 mkpath = require('mkpath'),
 path = require('path'),
-request = require('request');
-
-
-     console.log("   dbg: past declarations");
+request = require('request');                                    
+                                                                console.log("   line 11: past declarations");
 
 function importFromTransifex(options) // 9
-{
-       console.log('   dbg: options: ' + options); 
+{                                                                 console.log('\n   line 13: options --> ' + options); 
 
-
-
-
-  var authHeader = 'Basic' + new Buffer(options.user).toString('base64'); // 7
-       console.log('   dbg: authHeader: ' + authHeader);
-
-
-
+  var authHeader = 'Basic' + new Buffer(options.user).toString('base64');  // 7
+                                                                 console.log('   line 16: authHeader --> ' + authHeader);
   function writeFile(relPath, exports, callback)      
-  {                                                        console.log('   \ndbg: in writeFile(): relPath: ' + relPath + '; exports:' + exports + '; callback:'+callback);
+  {                                                              console.log('   \nline 18: in writeFile(): relPath --> ' + relPath + '; exports --> ' + exports + '; callback --> '+callback);
     callback = callback || function(){};
-    var absPath = path.join(options.dir, relPath);
-
-        console.log('   dbg: absPath: ' + absPath);
+    var absPath = path.join(options.dir, relPath);               console.log('   line 21: absPath --> ' + absPath);
 
 
-
-    mkpath(path.dirname(absPath), function (err)
+    mkpath(path.dirname(absPath), function ( err ) // 11 ?
     {
       if (err)
       {
@@ -48,22 +37,22 @@ function importFromTransifex(options) // 9
 
   function projectRequest (url, callback) // 5
   {
-    request.get(  { url:url, headers:{'Authorization': authHeader} }, function (error, response, body) // 8
-    {                                                  console.log('   \ndbg: in function projectRequest(): \nurl: ' + url + '; \nauthHeader: '+authHeader+'; \nerror: ' + error + '; \nresponse: ' + response + '; \nresponse.statusCode: ' + response.statusCode +  '; \nbody: ' + body);
+    request.get(  { url:url, headers:{'Authorization': authHeader} }, function (error, response, body) // 8 ?
+    {                                                           console.log('\n   line 39: in function projectRequest(): \nurl --> ' + url + '; \nauthHeader --> '+authHeader+'; \nerror --> ' + error + '; \nresponse --> ' + response + '; \nresponse.statusCode --> ' + response.statusCode +  '; \nbody --> ' + body);
       if (error)
       {
         callback(error);
       }
 
       if (response.statusCode !== 200)
-      {                                                                       console.log('   dbg: in the if(response.statusCode !==200)');
-        callback(Error (url + " returned " + response.statusCode));  // 10
+      {                                                          console.log('   line 47: in the if(response.statusCode !==200)');
+        callback( Error (url + " returned " + response.statusCode));  // 10
       }
 
       callback(null, body);
 
     }); // request.get()
-  } // projectRequest()
+  }; // projectRequest() // 12
 
 
   var detailsPath = '/?details',
@@ -72,9 +61,8 @@ function importFromTransifex(options) // 9
 
 
 
-  projectRequest(url, function (error, projectDetails) // 4
-  {
-         console.log('   \ndbg: in projectRequest(): \nerror: >>'+ error +'<<; \nprojectDetails: ' + projectDetails);
+  projectRequest(url, function (error, projectDetails) // 4 ?
+  {                                                                console.log('\n   line 63: in projectRequest(): \nerror --> '+ error +'; \nprojectDetails --> ' + projectDetails);
 
     if (error)
     {
@@ -82,25 +70,25 @@ function importFromTransifex(options) // 9
     }
 
     var resources =  JSON.parse(projectDetails); // 6
-         console.log('   dbg: resources: ' + resources);
+                                                                    console.log('   line 71: resources: ' + resources);
 
-    resources.teams.forEach(function (entry)
+    resources.teams.forEach(function (entry) //?
     {
       resourcePath = resources.resources[0].slug + '/translation/' + entry;
       var url = BASE_URL + options.project + '/resource/' + resourcePath + '/?file';
 
-      projectRequest (url, function (error, fileContent)
+      projectRequest (url, function (error, fileContent) // ?
       {
         if (error)
         {
-          return console.log("   dbg: I cyan return the fileContent");
+          return console.log("Cannot return the fileContent");
         }
 
         var filename = entry + '.plist';
 
-        writeFile(filename, fileContent, function (err)
+        writeFile(filename, fileContent, function (err) //?
         {
-          console.log( (err? "Error writing " : "Wrote ") + filename);
+          console.log( (err ? "Error writing " : "Wrote ") + filename); // 13
         });// writeFile()
 
       }); // projectRequest()
@@ -117,30 +105,29 @@ function importFromTransifex(options) // 9
 
 
 function main()
-{
-       console.log('   dbg: entered main()');
+{                                                                          console.log('   line 107: entered main()');
   var program = require ('commander');
 
   program
-  .option ('-u, --user <user:pass>', 'Specify your username and password on Transifex like this: John:abc123')
-  .option ('-p --project <slug>', 'Specify project slug')
-  .option ('-d --dir <path>', 'locale directory for the downloaded .plist files')
-  .parse(process.argv);
-
+    .option ('-u, --user <user:pass>', 'Specify your username and password on Transifex like this: John:abc123')
+    .option ('-p, --project <slug>', 'Specify project slug')
+    .option ('-d, --dir <path>', 'locale directory for the downloaded .plist files')
+    .parse(process.argv);
+                                                                               console.log('   line 115: past program options declarations');
   if (!program.user)
   {
     console.log('Please specify credentials with "-u user:pw".');
     process.exit(1);
   }
 
-  program.project = program.project || DEFAULT_PROJECT;
-  program.dir = program.dir || DEFAULT_DIR;
-  importFromTransifex(program);
+  program.project = program.project || DEFAULT_PROJECT;                             console.log('   line 122: program.project --> '+program.project);
+  program.dir = program.dir || DEFAULT_DIR;                                          console.log('   line 123: program.dir --> ' + program.dir);
+  importFromTransifex(program);                                               console.log('   line 124: started importFromTransifex()');
 
 } // main()
 
 if (!module.parent)
-{
+{                                                                                 console.log('   line 128: entered the if (!module.parent)');
   main();
 }
 
@@ -160,5 +147,11 @@ if (!module.parent)
     body value is "Authorization required"
   9) options value is [object Object]
   10) Create an Error object. Error() is a constructor of an Error object? (part of node.js.)
+  11) why is $err orange, not white! 
+  12) end with semicolon (or without?)
+  13) error[space]? (...)
 
 */
+
+
+
